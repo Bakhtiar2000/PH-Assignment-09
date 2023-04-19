ee3d0094196ecf0b7e1e5b7e7ee9437d3eed0c6f
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-04-19 22:38:31 +0600

    added fake data

---
+++ src/Components/FeaturedJobs/FeaturedJobs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './FeaturedJobs.css'
+
+const FeaturedJobs = () => {
+    return (
+        <div>
+            <div>
+                <p>Featured jobs</p>
+            </div>
+        </div>
+    );
+};
+
+export default FeaturedJobs;
