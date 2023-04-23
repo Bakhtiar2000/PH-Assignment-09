af19f7fca8bd74f7e52af772535bb7740c86dc54
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-04-23 14:19:27 +0600

    added filter button

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from 'react';
 import './AppliedJobs.css'
 import { useLoaderData, useParams } from 'react-router-dom';
 import AppliedJob from '../AppliedJob/AppliedJob';
+import { ChevronDownIcon } from '@heroicons/react/24/solid'
 
 const AppliedJobs = () => {
     const params = useParams();
     const jobInfo = useLoaderData();
 
     const [job, setJob] = useState({})
-    const [appliedJob, setAppliedJob]= useState([])
+    const [appliedJob, setAppliedJob] = useState([])
 
     useEffect(() => {
         fetch('/jobs.json')
@@ -22,16 +23,16 @@ const AppliedJobs = () => {
 
     const handleAddToCart = (job) => {
 
-        let newAppliedJob=[];
+        let newAppliedJob = [];
 
-        const existing= appliedJob.find(p=> p.id ===job.id);
-        if(!existing){
-            job.quantity= 1
+        const existing = appliedJob.find(p => p.id === job.id);
+        if (!existing) {
+            job.quantity = 1
             newAppliedJob = [...AppliedJobs, job];
         }
-        else{
-            existing.quantity= existing.quantity +1;
-            const remaining= appliedJob.filter(p=> p.id!== job.id)
+        else {
+            existing.quantity = existing.quantity + 1;
+            const remaining = appliedJob.filter(p => p.id !== job.id)
             newAppliedJob = [...remaining, existing];
         }
         setAppliedJob(newAppliedJob);
@@ -44,13 +45,23 @@ const AppliedJobs = () => {
         <div>
             <h2 className='Details-heading'>Applied Jobs</h2>
             <div className='applied-jobs-container'>
-            {
-                <AppliedJob
-                    key={appliedJob.id}
-                    job={job}
-                ></AppliedJob>
-            }
-            </div>           
+                <div className='filter'>
+                <div className="dropdown">
+                    <button className="drop-btn">Filter By <ChevronDownIcon className='icon'/>
+                    </button>
+                    <div className="dropdown-content">
+                        <a href="#">Remote</a>
+                        <a href="#">Full Time</a>
+                    </div>
+                </div>
+                </div>
+                {
+                    <AppliedJob
+                        key={appliedJob.id}
+                        job={job}
+                    ></AppliedJob>
+                }
+            </div>
         </div>
     );
 };
